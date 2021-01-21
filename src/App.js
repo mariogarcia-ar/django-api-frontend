50ac5c27c00ce9cc1dead578250e4bdf73674b4f
Mario Garcia <mariogarcia.ar@gmail.com>
2021-01-21 15:51:56 -0300

    Add Mock Data. Issue #1.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {id:1, title:'1st title', body:'body1'},
+  {id:2, title:'2st title', body:'body2'},
+  {id:3, title:'3st title', body:'body3'}
+];
+
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {list};
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.list.map( item => (
+          
+          <div key={item.id} >
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+          </div>
+        )
+
+        )}
+      </div>
+    )
+  }
 }
 
 export default App;
